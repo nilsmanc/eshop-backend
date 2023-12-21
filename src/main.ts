70f1a68392b71ece91f5d2b66d58082d5c5bd570
nilsmanc <etereaiberia@gmail.com>
2023-12-21 07:50:34 +0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import * as passport from 'passport';
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,16 @@ async function bootstrap() {
   );
   app.use(passport.initialize());
   app.use(passport.session());
+
+  const config = new DocumentBuilder()
+    .setTitle('Eshop')
+    .setDescription('api documentation')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
