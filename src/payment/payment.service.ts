ee3076a8ad46f7bea449c31a1edd13d448cb8407
nilsmanc <etereaiberia@gmail.com>
2024-01-03 09:04:39 +0300

    payment service

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { MakePaymentDto } from './dto/make-payment.dto';
+import { CheckPaymentDto } from './dto/check-payment.dto';
 
 @Injectable()
-export class PaymentService {}
+export class PaymentService {
+  async makePayment(makePaymentDto: MakePaymentDto) {
+    try {
+      const { data } = await axios({
+        method: 'POST',
+        url: 'https://api.yookassa.ru/v3/payments',
+        headers: {
+          'Content-Type': 'application/json',
+          'Idempotence-Key': Date.now(),
+        },
+        auth: {
+          username: process.env.PAYMENT_USERNAME,
+          password: process.env.PAYMENT_PASSWORD,
+        },
+        data: {
+          amount: {
+            value: makePaymentDto.amount,
+            currency: 'RUB',
+          },
+          capture: true,
+          confirmation: {
+            type: 'redirect',
+            return_url: 'http://localhost:3001/order',
+          },
+          description: makePaymentDto.description,
+        },
+      });
+
+      return data;
+    } catch (error) {
+      throw new ForbiddenException(error);
+    }
+  }
+
+  async checkPayment(checkPaymentDto: CheckPaymentDto) {
+    try {
+      const { data } = await axios({
+        method: 'GET',
+        url: `https://api.yookassa.ru/v3/payments/${checkPaymentDto.paymentId}`,
+        auth: {
+          username: process.env.PAYMENT_USERNAME,
+          password: process.env.PAYMENT_PASSWORD,
+        },
+      });
+
+      return data;
+    } catch (error) {
+      throw new ForbiddenException(error);
+    }
+  }
+}
