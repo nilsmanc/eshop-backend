0fe1d77f3eb4bdae57aef7bd31e3f29031cbf855
nilsmanc <etereaiberia@gmail.com>
2024-01-04 07:50:00 +0300

    payment controller

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { PaymentService } from './payment.service';
+import { MakePaymentDto } from './dto/make-payment.dto';
+import { AuthenticatedGuard } from 'src/auth/authenticated.guard';
+
+import { CheckPaymentDto } from './dto/check-payment.dto';
 
 @Controller('payment')
-export class PaymentController {}
+export class PaymentController {
+  constructor(private paymentService: PaymentService) {}
+
+  @UseGuards(AuthenticatedGuard)
+  @Post()
+  makePayment(@Body() makePaymentDto: MakePaymentDto) {
+    return this.paymentService.makePayment(makePaymentDto);
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Post('/info')
+  checkPayment(@Body() checkPaymentDto: CheckPaymentDto) {
+    return this.paymentService.checkPayment(checkPaymentDto);
+  }
+}
